27792b3a2447de01cab0d2148d2284c38f603a7e
gabrielemarra <61741485+gabrielemarra@users.noreply.github.com>
2020-08-25 16:15:51 +0200

    Update project_reti_logiche.vhd

--- sources_1/new/project_reti_logiche.vhd
+++ sources_1/new/project_reti_logiche.vhd
@@ -1,31 +1,15 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
 -- 
--- Create Date: 22.07.2020 16:46:39
--- Design Name: 
--- Module Name: top_level - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
+-- Prova Finale Reti Logiche 2019/2020
+-- Gabriele Marra - Codice Persona: 10572983 - Matricola: 887167
+-- Matteo Miceli - Codice Persona: 10560901 - Matricola: 888156
 -- 
 ----------------------------------------------------------------------------------
 
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
 use IEEE.NUMERIC_STD.ALL;
-
 use ieee.std_logic_unsigned.all;
 
 entity project_reti_logiche is
@@ -46,25 +30,22 @@ architecture Behavioral of project_reti_logiche is
 
 type state_type is (START_STATE, WAITING_ADDRESS, READING_ADDRESS, WAITING_STATE, WZ_ANALYSIS, FOUND, NOT_FOUND, FINISHED);
 
+ -- SIGNALS --
 signal state : state_type;
 
-signal address_read : std_logic_vector(7 downto 0);
-signal counterVector : std_logic_vector(3 downto 0);
-signal counter : integer;
+signal wz_num : integer;
 signal address_to_code : std_logic_vector(7 downto 0);
-signal offset : std_logic_vector(3 downto 0);
+signal wz_offset : std_logic_vector(3 downto 0);
 
- -- SIGNALS --
+ -- CODE -- 
 begin
 	
 	process(i_clk, i_rst)
-	 -- Process variable --
 	begin
 		if i_rst = '1' then
 			state <= START_STATE;
 			o_done <= '0';
-			counterVector <= "0000";
-			counter <= 0;
+			wz_num <= 0;
 		elsif rising_edge(i_clk) then
 			case state is
 				when START_STATE =>
@@ -90,31 +71,31 @@ begin
 					state <= WZ_ANALYSIS;
 				when WZ_ANALYSIS =>
 					if(i_data = address_to_code) then
-						offset <= "0001";
+						wz_offset <= "0001";
 						state <= FOUND;
 					elsif ( i_data+1 = address_to_code) then
-						offset <= "0010";
+						wz_offset <= "0010";
 						state <= FOUND;
 					elsif ( i_data+2 = address_to_code) then
-						offset <= "0100";
+						wz_offset <= "0100";
 						state <= FOUND;
 					elsif ( i_data+3 = address_to_code) then
-						offset <= "1000";
+						wz_offset <= "1000";
 						state <= FOUND;
-					elsif (counter > 6) then
+					elsif (wz_num > 6) then
 						state <= NOT_FOUND;
 					else 
-						counter <= counter + 1; 
+						wz_num <= wz_num + 1; 
 						o_en <= '1';
 						o_we <= '0';
-						o_address <= std_logic_vector(to_unsigned(counter+1,o_address'length));
+						o_address <= std_logic_vector(to_unsigned(wz_num+1,o_address'length));
 						state <= WAITING_STATE;
 					end if;
 				when FOUND =>
 					o_en <= '1';
 					o_we <= '1';
 					o_address <= (0 =>'1',1 =>'0',2 =>'0',3 =>'1', others => '0');
-					o_data <= '1' & std_logic_vector(to_unsigned(counter,3)) & offset;
+					o_data <= '1' & std_logic_vector(to_unsigned(wz_num,3)) & wz_offset;
 					o_done <= '1';
 					state <= FINISHED;
 				when NOT_FOUND =>
@@ -128,8 +109,7 @@ begin
 					if(i_start = '0') then
 						state <= START_STATE;
 						o_done <= '0';
-						counterVector <= "0000";
-						counter <= 0;
+						wz_num <= 0;
 					end if;
 			end case;
 		end if;
